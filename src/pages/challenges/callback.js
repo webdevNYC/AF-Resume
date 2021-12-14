49dfe25c23c682ac70e02e344fc2939540daa3f0
Dali Commerce <support@dalicommerce.com>
2021-12-13 21:11:37 -0500

    callback format

--- src/pages/challenges/callback.js
+++ src/pages/challenges/callback.js
@@ -5,27 +5,35 @@ When the returned function is called the first time, it should call the callback
 
 If it is called any additional times, instead of calling the callback function again it will simply return the output value from the first time it was called.
 */
+export default function Callback() {
 
-function sum(x) {
-    return function (y) {
-        return x + y;
-    }
-}
-
-function once(callback) {
-    let result;
-    return function () {
-        if (callback) {
-            result = callback.apply(this, arguments);
-            callback = null;
+    function sum(x) {
+        return function (y) {
+            return x + y;
         }
-        return result;
     }
+
+    function once(callback) {
+        let result;
+        return function () {
+            if (callback) {
+                result = callback.apply(this, arguments);
+                callback = null;
+            }
+            return result;
+        }
+    }
+
+    /*** Uncomment these to check your work! ***/
+
+    const onceFunc = once(sum(5));
+    //console.log(onceFunc(4));  // => should log 9
+    //console.log(onceFunc(10));  // => should log 9
+    //console.log(onceFunc(9001)); // => should log 9
+
+    return (<div>
+        <div>{onceFunc(4)}</div>
+        <div>{onceFunc(10)}</div>
+        <div>  {onceFunc(9001)}</div>
+    </div>)
 }
-
-/*** Uncomment these to check your work! ***/
-
-const onceFunc = once(sum(5));
-console.log(onceFunc(4));  // => should log 9
-console.log(onceFunc(10));  // => should log 9
-console.log(onceFunc(9001)); // => should log 9
